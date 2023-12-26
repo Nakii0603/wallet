0f4be1bc09d89e7ccd5a08e7fcb4f2454212db67
Нацагдорж Мөнхбаяр <23LP2745@G303-CC0148.local>
2023-12-26 11:34:05 +0800

    add file

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,19 @@
+import { LogoIcon, LogoText } from "../../Components/Icons";
+
+export default function Home() {
+  return (
+    <div className="flex">
+      <div>
+        <div>
+
+        <LogoIcon />
+        <LogoText />
+        </div>
+        <div></div>
+
+      </div>
+
+      <div className="bg-[#0166FF] w-[50%] h-full "></div>
+    </div>
+  );
+}
